742fcd8eebfb649c161925b61d34189d8e7eda5d
BlizmanJakub <114649918+BlizmanJakub@users.noreply.github.com>
2022-11-03 10:03:25 +0100

    update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php echo '<p>First web on Azure</p>';
         echo '<h1>Blizman Jakub</h1>'; 
-        echo '<a href="https://blizmaniot.azurewebsites.net/index.php"> Link na github</a>';
+        echo '<a href="https://github.com/BlizmanJakub/blizman-iot"> Link na github</a>';
         ?>
