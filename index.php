dc557d29c6be6a376923855d3a183347f7cc74ab
BlizmanJakub <114649918+BlizmanJakub@users.noreply.github.com>
2022-11-03 10:01:18 +0100

    add link to github v2

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php echo '<p>First web on Azure</p>';
         echo '<h1>Blizman Jakub</h1>'; 
+        echo '<a href="https://blizmaniot.azurewebsites.net/index.php"> Link na github</a>';
         ?>
