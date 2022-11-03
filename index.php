809b4a515aca3b8ff1de4c8e32e69fe0adf5b941
BlizmanJakub <114649918+BlizmanJakub@users.noreply.github.com>
2022-11-03 09:56:47 +0100

    add github link

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php echo '<p>First web on Azure</p>';
-        echo '<h1>Blizman Jakub</h1>'; ?>
+        echo '<h1>Blizman Jakub</h1>'; 
+        <a href="https://github.com/BlizmanJakub/blizman-iot">Test $GET</a>
+        ?>
