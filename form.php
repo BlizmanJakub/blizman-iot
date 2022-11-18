b901b25ac57d6732a6d7c9afc0df506c19811540
BlizmanJakub <114649918+BlizmanJakub@users.noreply.github.com>
2022-11-18 12:00:33 +0100

    update form v3

--- form.php
+++ form.php
@@ -9,17 +9,42 @@
 <?php echo '<h2>Formular pre kontakotovanie zakaznikych oddeleni</h2>'; ?>
 
 <form method="post">  
-    Meno: <input type="text" name="name" value="<?php echo $na;?>">
+    Meno: 
+    <input type="text" 
+            name="meno" 
+            required
+    >
     <br><br>
-    E-mail: <input type="email" name="email" value="<?php echo $email;?>">
+    E-mail: 
+    <input type="email" 
+            name="email"
+            required
+    >
     <br><br>
-    Telefon: <input type="text" name="website" value="<?php echo $website;?>">
+    Telefon: 
+    <input type="text" 
+            name="telefon"
+    >
     <br><br>
     Jedna sa o nakup pre firmu?
-    <input type="radio" name="odpoved" <?php if (isset($odpoved) && $odpoved=="ano") echo "checked";?> value="">Ano
-    <input type="radio" name="odpoved" <?php if (isset($odpoved) && $odpoved=="nie") echo "checked";?> value="viac men">Nie
+    <input type="radio" 
+            name="odpoved" 
+            value="ano"
+    >Ano
+
+    <br>
+    <input type="radio" 
+            name="odpoved" 
+            value="nie"
+    >Nie
     <br><br>
-    Ktore oddelenie chcete kontaktovat? <input type="text" id="oddelenie" name="oddelenie" list="oddelenia">
+    Ktore oddelenie chcete kontaktovat?
+    <input type="text" 
+            id="oddelenie" 
+            name="oddelenie" 
+            list="oddelenia"
+            required
+    >
         <datalist id="oddelenia">
             <option value="Zakaznicka podpora">
             <option value="Reklamacne oddelenie">
@@ -27,7 +52,11 @@
             <option value="Sklad">
         </datalist>
     <br>
-    Poznamka: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+    Poznamka: 
+    <textarea name="poznamka" 
+            rows="5" 
+            cols="40">
+    </textarea>
     <br><br>
     <input type="submit" value="Odoslat">  
 </form>
