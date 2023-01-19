5b4de4aa650d94e1e7df7fb7254f63825bc5673a
BlizmanJakub <114649918+BlizmanJakub@users.noreply.github.com>
2023-01-19 13:41:33 +0100

    add txt

--- index2.php
+++ index2.php
@@ -1,6 +1,6 @@
 <?php
     echo '<h1> cisla.php .txt</h1>';
-    $file = fopen("cisla.txt", "a");
+    $file = fopen("cisla1.txt", "w");
     fwrite($file, "1 2 3".PHP_EOL);
     fwrite($file, "10 3 4");
     fclose($file);
